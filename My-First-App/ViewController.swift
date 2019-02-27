b40ec6105c57d7ab031ff0a461e39af54cc34841
Ricardo <eehaha@gmail.com>
2019-02-28 06:57:26 +0800

    Initial Commit

---
+++ My-First-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My-First-App
+//
+//  Created by Ricardo Hui on 28/2/2019.
+//  Copyright © 2019 Ricardo Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
