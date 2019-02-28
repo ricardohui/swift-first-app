b7d5cd74c7591e148002a9563a860594805af4ce
Ricardo <eehaha@gmail.com>
2019-03-01 06:52:49 +0800

    A Greeter project that demo the use of TextInput, UILabel and Button in iOS App

--- My-First-App/ViewController.swift
+++ My-First-App/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        
+        if let name=textView.text {
+            label.text="Hello "+name
+        }
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-
+    
+    
 }
 
